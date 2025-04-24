5b2f5be871a0be0b41d508c917b3c574cc984c97
Janny <jamycjycy@gmail.com>
2025-04-24 22:38:20 +0100

    commit do Iane

--- src/individual.py
+++ src/individual.py
@@ -1,16 +1,35 @@
 import random
+from fitness import calculate_total_fitness
+from utils import load_relationship_matrix
 
 NUM_GUESTS = 64
 NUM_TABLES = 8
 GUESTS_PER_TABLE = NUM_GUESTS // NUM_TABLES
 
+csv_path = "../data/seating_data.csv"
+relationship_matrix = load_relationship_matrix(csv_path)
+
 # Cria um array random com as mesas
 def generate_random_seating():
-    guests = list(range(64))
+    guests = list(range(NUM_GUESTS))
     random.shuffle(guests)
     seating = []
     
-    for i in range(0, 64, 8):
-        seating.append(guests[i:i+8])
+    for i in range(0, NUM_GUESTS, NUM_TABLES):
+        seating.append(guests[i:i+NUM_TABLES])
     
     return seating
+
+
+
+class Individual:
+    def __init__(self, seating=None):
+        if seating is None:
+            seating = generate_random_seating()
+        self.seating = seating
+        self._fitness = None
+
+    def fitness(self):
+        if self._fitness is None:
+            self._fitness = calculate_total_fitness(self.seating, relationship_matrix)
+        return self._fitness
---
+++ src/selection.py
@@ -0,0 +1,45 @@
+import random
+from copy import deepcopy
+from individual import Individual
+
+#roulette selection
+def roulette_selection(population: list[Individual]):
+
+    fitness_values = [ind.fitness() for ind in population] # get fitness values from population
+
+    total_fitness = sum(fitness_values)  # calculate total fitness
+    
+    # Generate random number between 0 and total fitness
+    random_nr = random.uniform(0, total_fitness)
+    box_boundary = 0
+    # For each individual check if random number is inside the individuals box
+    for ind, fitness in zip(population, fitness_values):
+        box_boundary += fitness
+        if random_nr <= box_boundary:  # if inside, return (deepcopy) of this individual
+            return deepcopy(ind)
+
+
+#ranking selsction
+def ranking_selection(population: list[Individual]):
+    
+    sorted_population = sorted(population, key=lambda ind: ind.fitness(), reverse=True)  #sort population from worst to best (by fitness)
+    n = len(sorted_population) # length of ranking
+
+    ranks = list(range(1, n + 1))  # list of range values [1, 2, 3, ...]
+
+    total_rank_sum = sum(ranks) # sum of ranks
+    probabilities = [rank / total_rank_sum for rank in ranks] # calculate probabilities for each individual
+
+    selected_index = random.choices(range(n), weights=probabilities, k=1)[0]  # select random individual based on probabilites
+    return deepcopy(sorted_population[selected_index]) # return this individual
+
+#tournament selection
+def tournament_selection(population: list[Individual], k=3): # k = 3 --> tournament with 3 competitors (recommended value for this data size)
+
+    # randomly choose competitors (with replacement, in order to fulfill one of the principles of selection algorithms, that all individuals even the one with
+    #    worst fitness have a chance higher than 0 to be selected. Without replacement, the one with lowest total fitness would always lose against its
+    #        competitors, so no chance of selection. With replacement, there is a probability > 0 that the total worst individual based on fitness gets randomly
+    #           choosen 3 times for the tournament, meaning it would be selected.
+    competitors = [random.choice(population) for n in range(k)]
+    best = max(competitors, key=lambda ind: ind.fitness()) # find competitor with highest fitness
+    return deepcopy(best) # return this individual
